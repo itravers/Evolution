a039ed1f1313548e193f7e5f07adcf48efb85939
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-08-29 15:53:57 -0700

    Creating creatures now works.
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/mygdx/game/Creature.java
+++ core/src/com/mygdx/game/Creature.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
@@ -20,12 +21,14 @@ public class Creature {
     private float angularDamping = .5f;
     private float density = 1.25f;
     private float friction = .5f;
+    private Vector2 size;
 
     //Collision Category
     public static final short CATEGORY = -1;
 
     public Creature(MyGdxGame parent, World physicsWorld, Vector2 position, Vector2 size){
         this.parent = parent;
+        this.size = size;
         setupPhysics(physicsWorld, position, size);
 
     }
@@ -33,14 +36,19 @@ public class Creature {
     private void setupPhysics(World physicsWorld, Vector2 position, Vector2 size){
         bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(position);
+        //bodyDef.position.set(position);
+        bodyDef.position.set((position.x - (Gdx.graphics.getWidth() /2) + size.x / 2)/parent.PIXELS_TO_METERS, (-position.y + (Gdx.graphics.getHeight() / 2 ) - size.y / 2)/parent.PIXELS_TO_METERS);
+        //bodyDef.position.set((position.x + size.x / 2) / parent.PIXELS_TO_METERS,
+                             //(position.y + size.y / 2) / parent.PIXELS_TO_METERS);
 
         body = physicsWorld.createBody(bodyDef);
         body.setLinearDamping(linearDamping);
         body.setAngularDamping(angularDamping);
 
         shape = new PolygonShape();
-        shape.setAsBox((size.x /2) / parent.PIXELS_TO_METERS, (size.y/2)/parent.PIXELS_TO_METERS);
+        shape.setAsBox((size.x /2)/parent.PIXELS_TO_METERS, (size.y / 2)/parent.PIXELS_TO_METERS);
+        //shape.setAsBox((size.x / 2) / parent.PIXELS_TO_METERS,
+                      // (size.y / 2) / parent.PIXELS_TO_METERS);
 
         fixtureDef = new FixtureDef();
         fixtureDef.filter.groupIndex = CATEGORY;
@@ -53,7 +61,34 @@ public class Creature {
         shape.dispose();
     }
 
-    public void render(){
+    public void update(){
+        //get input and apply inpulses.
+    }
 
+    public Vector2 getPos(){
+        //Vector2 pos = new Vector2((body.getPosition().x - (Gdx.graphics.getWidth() /2) + size.x / 2)*parent.PIXELS_TO_METERS, body.getPosition().y);
+        float x = body.getPosition().x;
+        float x2 = x * parent.PIXELS_TO_METERS;
+        float x3 = x2 - (size.x / 2);
+        float x4 = x3 + Gdx.graphics.getWidth()/2;
+
+        //System.out.println("x: " + x);
+        //System.out.println("x2: " + x2);
+        //System.out.println("x3 " + x3);
+        //System.out.println("x4 " + x4);
+
+        float y = body.getPosition().y;
+        float y2 = y * parent.PIXELS_TO_METERS;
+        float y3 = y2 + (size.y / 2);
+        float y4 = -(y3 - Gdx.graphics.getHeight()/2);
+
+       /// System.out.println("y: " + y);
+       // System.out.println("y2: " + y2);
+       // System.out.println("y3 " + y3);
+        //System.out.println("y4 " + y4);
+
+        Vector2 pos = new Vector2(x4 , y4);
+        //System.out.println("Graphics width: " + (Gdx.graphics.getWidth()/2));
+        return pos;
     }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -12,6 +12,8 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
+import java.util.ArrayList;
+
 public class MyGdxGame extends ApplicationAdapter {
 	World physicsWorld;
 
@@ -20,6 +22,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
+
+	ArrayList<Creature> creatures;
 
 	final float PIXELS_TO_METERS = 100f;
 	
@@ -30,23 +34,47 @@ public class MyGdxGame extends ApplicationAdapter {
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		createCreatures();
+	}
+
+	private void createCreatures(){
+		creatures = new ArrayList<Creature>();
+
+
+		//create a single creature for testing
+		Vector2 position = new Vector2(17, 12);
+		Vector2 size = new Vector2(50 , 50);
+		Creature c = new Creature(this, physicsWorld, position,  size);
+		System.out.println("POS: " + (int)c.getPos().x + ":" + c.getPos().y);
+		creatures.add(c);
 	}
 
 	@Override
 	public void render () {
 		camera.update();
+		updateCreatures();
 		physicsWorld.step(1f/60f,6, 2);
+
+
+
+
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
 
 		debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS, PIXELS_TO_METERS, 0);
-
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//debugMatrix = batch.getProjectionMatrix();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
 
 		debugRenderer.render(physicsWorld, debugMatrix);
+	}
+
+	private void updateCreatures(){
+		for(int i = 0; i < creatures.size(); i++){
+			creatures.get(i).update();
+		}
 	}
 	
 	@Override
