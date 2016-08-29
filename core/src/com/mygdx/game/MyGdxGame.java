05721364eb99900050fc47889ac918146bed182c
Isaac Assegai <Isaac.A.Travers@gmail.com>
2016-08-29 14:22:17 -0700

    Created Physics world
    Signed-off-by: Isaac Assegai <Isaac.A.Travers@gmail.com>

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,10 +7,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class MyGdxGame extends ApplicationAdapter {
+	World physicsWorld;
+
 	Box2DDebugRenderer debugRenderer;
+	Matrix4 debugMatrix;
 	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
@@ -19,6 +25,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
+		physicsWorld = new World(new Vector2(0, 0), true);
 		debugRenderer = new Box2DDebugRenderer();
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch = new SpriteBatch();
@@ -28,16 +35,26 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		camera.update();
+		physicsWorld.step(1f/60f,6, 2);
+		batch.setProjectionMatrix(camera.combined);
+
+		debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS, PIXELS_TO_METERS, 0);
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
+		debugRenderer.render(physicsWorld, debugMatrix);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		physicsWorld.dispose();
+		debugRenderer.dispose();
+
 		img.dispose();
 	}
 }
